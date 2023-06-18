5e556a1514db0749baf614c4182029b4defffc59
Luis Vieira <luisernani87@gmail.com>
2023-06-18 14:55:25 -0300

    hamburger manu done. need test

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
+// NavBar Shrink
 window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    var scrollPosition = window.screenY || document.documentElement.scrollTop;
+  var navbar = document.querySelector('.navbar');
+  var scrollPosition = window.screenY || document.documentElement.scrollTop;
+
+  if (scrollPosition > 0) {
+    navbar.classList.add('shrink');
+  } else {
+    navbar.classList.remove('shrink');
+  }
+});
   
-    if (scrollPosition > 0) {
-      navbar.classList.add('shrink');
-    } else {
-      navbar.classList.remove('shrink');
-    }
-  });
-  
+
+var navbar = document.querySelector('.navbar');
+var content = document.querySelector('.content');
+
+function adjustContentTop() {
+  var navbarHeight = navbar.offsetHeight;
+  content.style.paddingTop = navbarHeight + 'px';
+}
+
+window.addEventListener('load', adjustContentTop);
+window.addEventListener('resize', adjustContentTop);
+
+// Hamburger Menu
+const hamburgerBtn = document.querySelector('.hamburger')
+const navbarMobile = document.querySelector('.navbar-mobile')
+
+hamburgerBtn.addEventListener('click', ()=>{
+  hamburgerBtn.classList.toggle('active')
+  navbarMobile.classList.toggle('active');
+})
