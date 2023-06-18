6e22241b9dc2fa5cf5e0741d9d90da0e6640b7fc
Luis Vieira <luisernani87@gmail.com>
2023-06-17 23:16:15 -0300

    responsive done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
-    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var scrollPosition = window.screenY || document.documentElement.scrollTop;
   
     if (scrollPosition > 0) {
       navbar.classList.add('shrink');
